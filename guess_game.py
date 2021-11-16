db6ed557b0782d40880859e68f7a3c6cdbfb4477
SzerokiFrEdzio <piotr.siecinski@gmail.com>
2021-11-16 11:45:19 +0100

    Guess the number game from 0 to 10

---
+++ guess_game.py
@@ -0,0 +1,18 @@
+import random
+while True:
+    print("Guess what number I'm thinking of... ")
+    rn = random.randrange(11)
+    print(rn)
+    choice = int(input('Guess: '))
+    if choice == rn:
+        print('Mindreader...')
+    elif choice > rn:
+        print('Too much')
+    elif choice < rn:
+        print('Not enough')
+    next = input('One more time? y/n ')
+    if next == 'y':
+        continue
+    if next == 'n':
+        print('bye')
+    break
